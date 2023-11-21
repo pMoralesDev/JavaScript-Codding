e2dfab43f99905b18827804edff1690d56a0e836
pMoralesDev <pMoralesDev@gmail.com>
2023-11-21 17:36:36 +0100

    23-11-21/#6ThreeInLine-Ending

--- #06 - Three in Line/index.js
+++ #06 - Three in Line/index.js
@@ -2,7 +2,7 @@ var playerTurn, moves, isGameOver, span, restartButton
 playerTurn = "x"
 moves = 0
 isGameOver = false
-span = document.getElementsByClassName("span")
+span = document.getElementsByTagName("span")
 restartButton = '<button onclick="playAgain()"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-repeat" viewBox="0 0 16 16"><path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z"/><path fill-rule="evenodd" d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z"/></svg></button>'
 
 function play (y){
@@ -34,10 +34,10 @@ function checkWinner(a,b,c){
     b--
     c--
     if((span[a].dataset.player===span[b].dataset.player)&&(span[b].dataset.player === span[c].dataset.player)
-    &&(span[a].dataset.player === span[c].dataset.player)&&(span[a].dataset.player||span[a].dataset.player)&& isGameOver==false){
-        span[a].parentNode.className += "activeBox"
-        span[b].parentNode.className += "activeBox"
-        span[c].parentNode.className += "activeBox"
+    &&(span[a].dataset.player === span[c].dataset.player)&&(span[a].dataset.player === "x" ||span[a].dataset.player === "o")&& isGameOver==false){
+        span[a].parentNode.className += " active-box"
+        span[b].parentNode.className += " active-box"
+        span[c].parentNode.className += " active-box"
         gameOver(a)
     }
 }
@@ -46,8 +46,8 @@ function playAgain() {
     document.getElementsByClassName("alert")[0].parentNode.removeChild(document.getElementsByClassName("alert")[0])
     resetGame()
     window.isGameOver = false
-    for( let i = 0; i < span.lengyj; i++){
-        span[i].parentNode.className = span[i].parentNode.className.replace("activeBox", "")
+    for( let i = 0; i < span.length; i++){
+        span[i].parentNode.className = span[i].parentNode.className.replace("active-box", "")
     }
 }
 
@@ -60,7 +60,7 @@ function resetGame() {
 }
 
 function gameOver(a) {
-    let gameOverAlertElement = "<b>GAME OVER</b><br><br> Player "+ span [a].dataset.player.toUpperCase()+" WIN!!!<br><br>"+restartButton
+    let gameOverAlertElement = "<b>GAME OVER</b><br><br> Player "+ span[a].dataset.player.toUpperCase()+" WIN!!!<br><br>"+restartButton
     let div = document.createElement("div")
     div.className = "alert"
     div.innerHTML = gameOverAlertElement
