ecf09bf70721fee9fd49c0110a85e9e428731728
pMoralesDev <pMoralesDev@gmail.com>
2023-12-03 11:28:50 +0100

    23-12-03/#1Tetris-Ending

--- Games/#1 - Tetris/index.js
+++ Games/#1 - Tetris/index.js
@@ -118,7 +118,7 @@ function merge(arena, player){
 
 function rotate(matrix, dir){
     for(let y=0; y<matrix.length; ++y){
-        for(let x; x<y;++x){
+        for(let x=0; x<y;++x){
             [matrix[x][y],matrix[y][x]]=[matrix[y][x],matrix[x][y]]
         }
     }
