4035ac8542a741e87994777a3e16096ecbf88995
pMoralesDev <pMoralesDev@gmail.com>
2023-12-18 18:24:53 +0100

    23-12-18/#02TextEdito-Comments

--- Programming challenges/#02 - Text Editor/index.js
+++ Programming challenges/#02 - Text Editor/index.js
@@ -1,3 +1,15 @@
+/**
+ * @param optionsButtons referencia a todos los botenes presentes en la interfaz
+ * @param advancedOptionsButtons referencia a todos los elementos con opciones multiples de elección de efectos paa el texto
+ * @param fontName referencia directamente al elemento que configura las fuentes del texto
+ * @param fontSizeRef referencia directamente al elemento que configura el tamaño del texto
+ * @param writingArea refencia al area donde escribe el usuario dentro de la apliación
+ * @param linkButton referencia directamente al botón que permite incluir un enlace en el texto
+ * @param alignButtons referencia a los botones que confiugran la alineación del texto (derecha, izquierda, justificado y centrado)
+ * @param spacingButtons referencia a los botones que configuran el espacio en las listas
+ * @param formatButtons referencia a los botones que configuran el formato del texto (negrita, cursiva...)
+ * @param scriptButtons refencia a los botones que configuran el superindice y subindice
+ */
 let optionsButtons = document.querySelectorAll(".option-button")
 let advancedOptionsButtons = document.querySelectorAll(".adv-option-button")
 let fontName = document.getElementById("fontName")
@@ -8,7 +20,9 @@ let alignButtons = document.querySelectorAll(".align")
 let spacingButtons = document.querySelectorAll(".spacing")
 let formatButtons = document.querySelectorAll(".format")
 let scriptButtons = document.querySelectorAll(".script")
-
+/**
+ * @param fontList lista de fuentes que va a usar la aplicación
+ */
 let fontList = [
     "Arial",
     "Verdana",
@@ -18,46 +32,66 @@ let fontList = [
     "Courier New",
     "Cursive"
 ]
-
+/**
+ * @function intializer función encargada de arranzar la apliación al cargar la página
+ */
 const intializer = () => {
     highLighter(alignButtons, true)
     highLighter(spacingButtons, true)
     highLighter(formatButtons, false)
     highLighter(scriptButtons, true)
-
+    /**
+     * Utilizamos la función map para convertir los elementos del array fontList en elementos option de html que puedan ser visualizados dentro del elemento select
+     * Con la función appendChild agregamos los elementos option creados al elemento select que permite elegir fuentes de texto
+     */
     fontList.map((value) => {
         let option = document.createElement('option')
         option.value = value
         option.innerHTML = value
         fontName.appendChild(option)
     })
-
+    /**
+     * Con un bucle for creamos 7 elementos option correspondientes a tamaños de fuente diferentes y los cargamos en el elemento select correspondiente con la función appendChild
+     */
     for(let i = 1; i <=7; i++) {
         let option = document.createElement("option")
         option.value = i
         option.innerHTML = i
         fontSizeRef.appendChild(option)
     }
-
+    /**
+     * Cargamos un tamaño de fuente por defecto
+     */
     fontSizeRef.value = 3
- }
-
+}
+/**
+ * @function modifyText
+ * @param {*} command elemento sobre el que se aplica la funcionalidad
+ * @param {*} defaultUi 
+ * @param {*} value 
+ */
 const modifyText = (command, defaultUi, value) => {
     document.execCommand(command, defaultUi, value)
 }
-
+/**
+ * Cargamos las funcionalidades de modificar texto sobre los elementos button de la interfaz
+ */
 optionsButtons.forEach((button) => {
     button.addEventListener("click", () => {
         modifyText(button.id, false, null)
     })
 })
-
+/**
+ * Cargamos las funcionalidades de moficar texto sobre los elementos con la clase adv-option-button de la interfaz
+ */
 advancedOptionsButtons.forEach((button) => {
     button.addEventListener("change", () => {
         modifyText(button.id, false, button.value)
     })
 })
-
+/**
+ * Cargamos la funcionalidad de modificar texto sobre el boton para insertar enlaces en el texto
+ */
 linkButton.addEventListener("click", () => {
     let userLink = prompt("Enter a URL")
     if (/http/i.test(userLink)){
@@ -67,7 +101,11 @@ linkButton.addEventListener("click", () => {
         modifyText(linkButton.id, false, userLink)
     }
 })
-
+/**
+ * @function highLighter
+ * @param {*} className clase sobre la que la función hará su funcionalidad
+ * @param {*} needsRemoval variable booleana para decir a la función si tiene que activar o no una clase
+ */
 const highLighter = (className, needsRemoval) => {
     className.forEach((button) => {
         button.addEventListener("click", () => {
@@ -86,12 +124,16 @@ const highLighter = (className, needsRemoval) => {
         })
     })
 }
-
+/**
+ * @function highLighterRemover 
+ */
 const highLighterRemover = (className) => {
     className.forEach((button) => {
         button.classList.remove("active")
     })
 }
-
+/**
+ * Comando para inicializar la aplicación
+ */
 window.onload = intializer()
 
