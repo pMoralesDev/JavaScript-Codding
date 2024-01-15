4889e259702cc9ea66deb9279f1512e79f48cc58
pMoralesDev <pMoralesDev@gmail.com>
2024-01-15 16:51:15 +0100

    24-01-15/#17BudgetApp-NoEnding

--- Programming challenges/#17 - Budget App/index.js
+++ Programming challenges/#17 - Budget App/index.js
@@ -8,6 +8,7 @@ const productTitleError = document.getElementById("product-title-error")
 const productCostError = document.getElementById("product-cost-error")
 const amount = document.getElementById("amount")
 const expenditureValue = document.getElementById("expenditure-value")
+const list = document.getElementById("list")
 const balanceValue = document.getElementById("balance-amount")
 let tempAmount = 0
 /**
@@ -24,7 +25,7 @@ totalAmountButton.addEventListener('click', () => {
         errorMessage.classList.add("hide")
         amount.innerHTML = tempAmount
         balanceValue.innerText = tempAmount - expenditureValue.innerText
-        totalAmount.value = 0
+        totalAmount.value = ""
     }
 })
 /**
@@ -42,6 +43,59 @@ const disableButtons = (bool) => {
 const modifyElement = (element, edit = false) => {
     let parentDiv = element.parentElement
     let currentBalance = balanceValue.innerText
-    let currentExpence = expenditureValue.innerText
+    let currentExpense = expenditureValue.innerText
     let parentAmount = parentDiv.querySelector(".amount").innerText
+    if(edit){
+        let parentText = parentDiv.querySelector(".product").innerText
+        productTitle.value = parentText
+        userAmount.value = parentAmount
+        disableButtons(true)
+    }
+    balanceValue.innerText = parseInt(currentBalance) + parseInt(parentAmount)
+    expenditureValue.innerText = parseInt(currentExpense) - parseInt(parentAmount)
+    parentDiv.remove()
 }
+/**
+ * Creamos la lista de funciones
+ */
+const listCreator = (expenseName, expenseValue) => {
+    let subListContent = document.createElement("div")
+    subListContent.classList.add("sublist-content", "flex-space")
+    list.appendChild(subListContent)
+    subListContent.innerHTML = `<p class='product'>${expenseName}</p><p class='amount'>${expenseValue}</p>`
+    let editButton = document.createElement("button")
+    editButton.classList.add("fa-solid", "fa-pen-to-square", "edit")
+    editButton.style.fontSize="1.2em"
+    editButton.addEventListener('click', () => {
+        modifyElement(editButton, true)
+    })
+    let deleteButton = document.createElement("button") 
+    deleteButton.classList.add("fa-solid", "fa-trash-can", "delete")
+    deleteButton.style.fontSize = "1.2em"
+    deleteButton.addEventListener('click', () => {
+        modifyElement(deleteButton)
+    })
+    subListContent.appendChild(editButton)
+    subListContent.appendChild(deleteButton)
+    document.getElementById("list").appendChild(subListContent)
+}
+/**
+ * Agregamos las funciones para gastos
+ */
+checkAmountButton.addEventListener('click', () => {
+    /**
+     * comprobamos si esta vacio
+     */
+    if(!userAmount.value || !productTitle.value){
+        productTitleError.classList.remove("hide")
+        return false
+    }
+    disableButtons(false)
+    let expenditure  = parseInt(userAmount.value)
+    let sum = parseInt(expenditureValue.innerText) + expenditure
+    expenditureValue.innerText = sum
+    balanceValue.innerText = totalBalance
+    listCreator(productTitle.value, userAmount.value)
+    productTitle.value=""
+    userAmount.value=""
+})
