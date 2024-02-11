eca3064d3802c1d158647467362a04c2a810e8d2
Morales Dev <pmoralesdev@gmail.com>
2024-02-11 16:59:14 +0100

    24-02-11/#5SimonSay-FixBugs

--- Games/#05 - Simon Say/index.js
+++ Games/#05 - Simon Say/index.js
@@ -25,4 +25,85 @@ const getRandomColor = (colorObj) => {
     const colorKeys = Object.keys(colorObj)
     return colorKeys[Math.floor(Math.random()*colorKeys.length)]
 }
+/**
+ * 
+ * @param {*} time 
+ * @returns 
+ */
+const delay  = async (time) => {
+    return await new Promise((resolve) => setTimeout(resolve, time))
+}
+/**
+ * 
+ */
+const generateRandomPath = async () => {
+    randomColors.push(getRandomColor(colorObj))
+    score = randomColors.length
+    isPathGenerating = true
+    await showPath(randomColors)
+}
+/**
+ * 
+ * @param {*} colors 
+ */
+const showPath = async (colors) => {
+    scoreEl.innerText = score
+    for(let color of colors){
+        const currentColor = document.querySelector(`.${color}`)
+        await delay(500)
+        currentColor.style.backgroundColor = colorObj[color].new
+        await delay(600)
+        currentColor.style.backgroundColor = colorObj[color].current
+        await delay(600)
+    }
+    isPathGenerating = false
+}
+/**
+ * 
+ */
+const endGame = () => {
+    resultEl.innerText = `<span> Your score: ${score}</span>`
+    resultEl.classList.remove('hide')
+    wrapperEl.classList.add('hide')
+    startBtn.innerText = 'Play again'
+    startBtn.classList.remove('hide')
+}
+/**
+ * 
+ */
+const resetGame = () => {
+    score = 0
+    clickCount = 0
+    randomColors = []
+    isPathGenerating = false
+    wrapperEl.classList.remove('hide')
+    containerEl.classList.add('hide')
+    generateRandomPath()
+}
+/**
+ * 
+ * @param {*} e 
+ * @returns 
+ */
+const handleColorClick = async (e) => {
+    if (isPathGenerating){
+        return false
+    }
+    if (e.target.classList.contains(randomColors[clickCount])){
+        e.target.style.backgroundColor = colorObj[randomColors[clickCount]].new
+        await delay(600)
+        e.target.style.backgroundColor = colorObj[randomColors[clickCount]].current
+        clickCount++
+        if(clickCount === score){
+            clickCount = 0
+            generateRandomPath()
+        }
+    } else {
+        endGame()
+    }
+}
 
+startBtn.addEventListener('click', resetGame)
+colorParts.forEach((color) => {
+    color.addEventListener('click', handleColorClick())
+})
