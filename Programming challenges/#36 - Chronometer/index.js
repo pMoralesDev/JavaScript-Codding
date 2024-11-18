f2e5127e9f687ddeb6873a84d2d3ee53b7d080ac
PMoralesDev <pmoralesdev@gmail.com>
2024-11-18 12:15:08 +0100

    24-11-18/#36Chronometer-FixingBugs

--- Programming challenges/#36 - Chronometer/index.js
+++ Programming challenges/#36 - Chronometer/index.js
@@ -1,11 +1,34 @@
-const chronometer = document.getElementsByClassName("input-timer")
+const chronometer = document.getElementsByClassName("input-timer")[0];
+const startButton = document.querySelector(".start");
+const finishButton = document.querySelector(".finish");
 
-const {timer} = rxjs
-const {map} = rxjs.operators
+const { timer, Subject } = rxjs;
+const { takeUntil } = rxjs.operators;
 
-timer(1000, 1000).pipe(map(()=>{
-    const ahora = new Date()
-    return ahora.toLocaleString()
-})).subscribe(valor=>{
-    chronometer.innerText = valor
-})
+const stopChronometer$ = new Subject();
+
+let secondsElapsed = 0;
+
+function formatTime(seconds) {
+  const hours = Math.floor(seconds / 3600).toString().padStart(2, "0");
+  const minutes = Math.floor((seconds % 3600) / 60).toString().padStart(2, "0");
+  const secs = (seconds % 60).toString().padStart(2, "0");
+  return `${hours}:${minutes}:${secs}`;
+}
+
+function startChronometer() {
+  stopChronometer$.next();
+  timer(0, 1000)
+    .pipe(takeUntil(stopChronometer$))
+    .subscribe(() => {
+      secondsElapsed++;
+      chronometer.innerText = formatTime(secondsElapsed);
+    });
+}
+
+function stopChronometer() {
+  stopChronometer$.next();
+}
+
+startButton.addEventListener("click", startChronometer);
+finishButton.addEventListener("click", stopChronometer);
