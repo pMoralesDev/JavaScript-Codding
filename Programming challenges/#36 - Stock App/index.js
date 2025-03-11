e6ddad3ddb9f5e98ec1a430ba70eca2b6289ab55
Pablo Morales <pmoralesdev@gmail.com>
2025-03-11 21:27:39 +0100

    25-03-11/#36StockApp-Working

---
+++ Programming challenges/#36 - Stock App/index.js
@@ -0,0 +1,7 @@
+const symbolInput = document.querySelector('#symbol')
+const stockList = document.querySelector('#stock-list')
+
+// Fecth to api and display de top 10
+function fecthTopStocks(){
+    
+}
