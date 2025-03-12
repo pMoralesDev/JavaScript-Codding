2bfd7ee12dd1b6463df7027bf20259b7592859c9
Pablo Morales <pmoralesdev@gmail.com>
2025-03-12 21:19:57 +0100

    25-03-12/#36StockApp-Working

--- Programming challenges/#37 - Stock App/index.js
+++ Programming challenges/#37 - Stock App/index.js
@@ -3,5 +3,25 @@ const stockList = document.querySelector('#stock-list')
 
 // Fecth to api and display de top 10
 function fecthTopStocks(){
-    
+    fetch('https://www.alphavantage.co/query?function=SECTOR&apikey=PPGAIPOEDBPR3Y5P').then(response => response.json()).then(data => {
+        const stocks = data['Rank A: Real-Time Performance']
+        let html = ''
+        // Generate html for each stock
+        for(let i = 0; i < 10; i++){
+            const symbol = Object.keys(stocks)[i]
+            const change = symbol[symbol]
+            const changeColor = parseFloat(change)>0 ? 'green' : 'red'
+            html += `
+                <li>
+                    <span class="symbol">${symbol}</span>
+                    <span class="change" style="color: ${changeColor}">${change}</span>
+                </li>
+            `
+        }
+        // Update the stok list
+        stockList.innerHTML = html
+    }).catch(error => {
+        console.log(error)
+    })
 }
+
